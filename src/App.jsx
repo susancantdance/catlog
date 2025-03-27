499146b4ee4b1eed61cef8a49324c857579fd796
susancantdance <susan.l.chen@gmail.com>
2025-03-27 13:32:31 -0700

    env var

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { Comments } from "./comments.jsx";
 import { Header } from "./header.jsx";
 import { BlogContext } from "./context.jsx";
-import env from "react-dotenv";
+// import env from "react-dotenv";
 
 import "./App.css";
 
@@ -12,7 +12,7 @@ function App() {
   // const [user, setUser] = useState({ id: "", username: "" });
   console.log("in the app");
   useEffect(() => {
-    fetch(env.URL + "/posts", {
+    fetch(import.meta.URL + "/posts", {
       method: "GET",
     })
       .then((response) => {
